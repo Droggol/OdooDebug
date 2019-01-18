f65d992d3029c1e33314916273b9b8a2183ecbab
Jigar Patel <jat@openerp.com>
2019-01-18 11:56:28 +0530

    [IMP] Added keyboard shortcuts, Imrove code, New cover images

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # OdooDebug
-Plugin for Quick Debug in Odoo
+Minimal and fast extension to toggle debug mode of <a href="https://www.odoo.com">Odoo</a>. It can be used via mouse and keyboard shortcuts.
+
+### How to use with mouse?
+- Click on extension for normal debug.
+- Double click on extension for debug with assets.
+
+### How to use with keyboard?
+- Press Ctrl + . for normal debug.
+- Press Ctrl + . + . for debug with assets. (Hold ctrl and press . twice)
+
+*use cmd ⌘ in mac*
 
 ### Installation
 - Chrome: install it from <a href="https://chrome.google.com/webstore/detail/odoo-debug/hmdmhilocobgohohpdpolmibjklfgkbi">chrome web store</a>
 - Firefox: install it from <a href="https://addons.mozilla.org/en-US/firefox/addon/odoo-debug/">AMO</a>
-
-### How to use?
-- Single click for normal debug
-- Double click for debug with assets
 
 ### License
 This is under licence <a href="https://github.com/pga-odoo/OdooDebug/blob/master/LICENSE">GNU Affero General Public License v3.0</a>
--- background.js
+++ background.js
@@ -15,63 +15,73 @@ var onClickListener = function (callback) {
                 callback.apply(this ,[tab, click]);
                 click = 0;
             }, CONTROL_TIME);
-
         };
     }
     throw new Error('[InvalidArgumentException]');
 };
 
-browser.browserAction.onClicked.addListener(
+var currentBrowser;
+if (navigator.userAgent.indexOf('Chrome') !== -1) {
+    currentBrowser = chrome;
+} else {
+    currentBrowser = browser;
+}
+
+function changeUrl (tab, click) {
+    var url = '';
+    var el = document.createElement('a');
+    el.href = tab.url;
+    if (el.search.indexOf('?debug') !== -1) {
+        if (click === 2 && el.search.indexOf('?debug=assets') === -1) {
+            url = el.origin + el.pathname + '?debug=assets' + el.hash;
+            currentBrowser.browserAction.setIcon({path: 'super_on.png'});
+        } else {
+            url = el.origin + el.pathname + el.hash;
+            currentBrowser.browserAction.setIcon({path: 'off.png'});
+        }
+    } else {
+        if (click === 1) {
+            url = el.origin + el.pathname + '?debug' + el.hash;
+            currentBrowser.browserAction.setIcon({path: 'on.png'});
+        } else {
+            url = el.origin + el.pathname + '?debug=assets' + el.hash;
+            currentBrowser.browserAction.setIcon({path: 'super_on.png'});
+        }
+    }
+    currentBrowser.tabs.update(tab.id, {url: url});
+}
+
+function changeIcon () {
+    currentBrowser.tabs.query({active: true, currentWindow: true}, function (tab) {
+        if (tab.length) {
+            var icon = 'off.png';
+            var el = document.createElement('a');
+            el.href = tab[0].url;
+            if (el.search.indexOf('?debug') !== -1) {
+                if (el.search.indexOf('?debug=assets') !== -1) {
+                    icon = 'super_on.png';
+                } else {
+                    icon = 'on.png';
+                }
+            }
+            currentBrowser.browserAction.setIcon({path: icon});
+        }
+    });
+}
+
+currentBrowser.browserAction.onClicked.addListener(
     new onClickListener(
         function onclick(tab, click) {
             changeUrl(tab, click);
         },
     )
 );
-
-function changeUrl (tab, click) {
-    var url = '';
-    var el = document.createElement('a');
-    el.href = tab.url;
-
-    if (el.search.indexOf('?debug') !== -1) {
-        if (click==2 && el.search.indexOf('?debug=assets') === -1) {
-            url = el.origin + el.pathname + '?debug=assets' + el.hash;
-            browser.browserAction.setIcon({'path': 'super_on.png'});
-        } else {
-            url = el.origin + el.pathname + el.hash;
-            browser.browserAction.setIcon({'path': 'off.png'});
-        }
-    } else {
-        if (click==1) {
-            url = el.origin + el.pathname + '?debug' + el.hash;
-            browser.browserAction.setIcon({'path': 'on.png'});
-        } else {
-            url = el.origin + el.pathname + '?debug=assets' + el.hash;
-            browser.browserAction.setIcon({'path': 'super_on.png'});
-        }
-    }
-
-    browser.tabs.update(tab.id, {'url': url});
-}
-
-function changeIcon () {
-    browser.tabs.query({active: true, currentWindow: true}, function (tab) {
-        var icon = 'off.png';
-        var el = document.createElement('a');
-        el.href = tab[0].url;
-        if (el.search.indexOf('?debug') !== -1) {
-            if (el.search.indexOf('?debug=assets') !== -1) {
-                icon = 'super_on.png';
-            } else {
-                icon = 'on.png';
-            }
-        }
-
-        browser.browserAction.setIcon({'path': icon});
-    });
-}
-
-browser.tabs.onActivated.addListener(function () { changeIcon(); });
-browser.tabs.onUpdated.addListener(function () { changeIcon(); });
-browser.windows.onFocusChanged.addListener(function () { changeIcon(); });
+currentBrowser.tabs.onActivated.addListener(function () {
+    changeIcon();
+});
+currentBrowser.tabs.onUpdated.addListener(function () {
+    changeIcon();
+});
+currentBrowser.windows.onFocusChanged.addListener(function () {
+    changeIcon();
+});
