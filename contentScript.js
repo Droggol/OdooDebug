5b13e50a200089afb6df73f829248de88c8e8c83
Jigar Patel <jat@openerp.com>
2019-08-16 23:41:47 +0530

    [REF] convert code to ES6

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,18 @@
-var currentBrowser;
+let currentBrowser;
 if (navigator.userAgent.indexOf('Chrome') !== -1) {
     currentBrowser = chrome;
 } else {
     currentBrowser = browser;
 }
 
-var scriptEl = document.createElement('script');
+const scriptEl = document.createElement('script');
 scriptEl.src = currentBrowser.extension.getURL('pageScript.js');
 (document.head || document.documentElement).appendChild(scriptEl);
-scriptEl.onload = function () {
-    scriptEl.parentNode.removeChild(scriptEl);
-};
+scriptEl.onload = () => scriptEl.parentNode.removeChild(scriptEl);
 
-currentBrowser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+currentBrowser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'get_info') {
-        var body = document.getElementsByTagName('body')[0];
+        const body = document.getElementsByTagName('body')[0];
         if (body) {
             sendResponse({
                 odoo_version: body.getAttribute('data-odoo-version'),
